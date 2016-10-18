e64775a3719d731f186c66ea66de542346b42e64
ashintr <ashintr@ncsu.edu>
2016-10-18 16:03:13 -0400

    Remove sample.txt

--- status.sh
+++ status.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl --user ashintr:amruta123 -s http://localhost:8080/job/job2/build 
+curl --user ashintr:amruta123 -s http://localhost:8080/job/job2/build
+
+curl http://localhost:8080/job/job2/lastBuild/api/json 
